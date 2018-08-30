f5747d371c91ec50e34d9dff1b4d40e2ccfc9e74
miracle <miracle@DESKTOP-KVLTBLB>
2018-08-30 16:29:35 +0800

    修复非付费用户无法获取评论问题 增加获取评论时候的用户付费信息。

--- xiangqi/src/cmd/CMD12.java
+++ xiangqi/src/cmd/CMD12.java
@@ -17,6 +17,7 @@ import main.ServerTimer;
 
 /**
  * 评论功能
+ * 
  * @author 周超 2018/8/25
  */
 public class CMD12 implements ICMD {
@@ -24,7 +25,8 @@ public class CMD12 implements ICMD {
 	private static final Map<Integer, Integer> praiseMap = new HashMap<Integer, Integer>();
 	private static final List<Integer> pralist = new ArrayList<Integer>();
 	private static final Map<Integer, String> comMap = new HashMap<Integer, String>();
-	private static final List<Integer> list = new ArrayList<Integer>();	
+	private static final List<Integer> list = new ArrayList<Integer>();
+
 	@Override
 	public ChannelBuffer getBytes(int code, ChannelBuffer data) {
 		ChannelBuffer buf = ChannelBuffers.dynamicBuffer();
@@ -34,23 +36,23 @@ public class CMD12 implements ICMD {
 		int deviceID = data.readInt();
 		String channel = Global.readUTF(data);
 		int boardNum = data.readInt();
-		log.info( name +"评论"+ ",davice = " + deviceID + ",boardNum=" + boardNum);
-		if (!"获取".equals(name)) {
-			Device device = Dao.getDeviceExist(deviceID, "");
-			if (device == null || device.getBuy() == 0) {
-				String msg = "提交".equals(name)?"您还未购买任何课程，不能评论。":"您还未购买任何课程，不能点赞。";
-				return backBuffer(buf, 2, msg);
-			}
+		log.info(name + "评论" + ",davice = " + deviceID + ",boardNum=" + boardNum);
+		Device device = Dao.getDeviceExist(deviceID, "");
+		if (device == null) {
+			return backBuffer(buf, 2, "找不到用户");
+		} else if (!"获取".equals(name) && device.getBuy() == 0) {
+			String msg = "提交".equals(name) ? "您还未购买任何课程，不能评论。" : "您还未购买任何课程，不能点赞。";
+			return backBuffer(buf, 2, msg);
 		}
-		if ("提交".equals(name)) {		
+		if ("提交".equals(name)) {
 			String userName = Global.readUTF(data);
 			String userAge = Global.readUTF(data);
 			String userMail = Global.readUTF(data);
 			String userContent = Global.readUTF(data).trim();
 			log.info("name=" + userName + ",age=" + userAge + ",联系方式=" + userMail);
-			if(userContent.equals(comMap.get(deviceID))){
+			if (userContent.equals(comMap.get(deviceID))) {
 				return backBuffer(buf, 2, "请勿重复提交。");
-			}			
+			}
 			Comment com = new Comment();
 			userName = ("用户昵称".equals(userName) ? "ID:" + deviceID : userName);
 			userMail = ("联系方式".equals(userMail) ? "未填" : userMail);
@@ -75,18 +77,17 @@ public class CMD12 implements ICMD {
 				Comment comment = Dao.getCommentByID(boardNum);// 当是点赞请求时，boardNum值是DeviceID
 				if (comment != null) {
 					comment.setPraise(comment.getPraise() + 1);
-					limitMapSize(praiseMap,pralist,deviceID);
+					limitMapSize(praiseMap, pralist, deviceID);
 					praiseMap.put(deviceID, num + 1);
 					Dao.save(comment);
 					return backBuffer(buf, 3, comment.getId() + "");
-				}else{
+				} else {
 					return backBuffer(buf, 2, "点赞的评论不存在。");
 				}
 			} else {
 				return backBuffer(buf, 2, "你已经超过了次数，每天限5次。谢谢你的参与!");
 			}
 		}
-
 		List<Comment> list = Dao.getComments(deviceID, boardNum);
 		StringBuilder sb = new StringBuilder();
 		int step = 0;
@@ -103,28 +104,33 @@ public class CMD12 implements ICMD {
 			}
 		}
 		html = "<Comments>" + sb.toString() + "</Comments>";
-		return backBuffer(buf,1, html);
+		String buy = device.getBuy() == 0 ? "未购" : "已购";
+		buf.writeByte(1);
+		buf.writeBytes(Global.getUTF(html));
+		buf.writeBytes(Global.getUTF(buy));
+		return buf;
 	}
-	/** 
+
+	/**
 	 * 限定map的长度。
 	 */
-	private void limitMapSize(Map map,List list,int deviceID) {
-		if(!map.containsKey(deviceID)){
-			list.add(deviceID);//控制map长度
-			if(list.size()>999){
+	private void limitMapSize(Map map, List list, int deviceID) {
+		if (!map.containsKey(deviceID)) {
+			list.add(deviceID);// 控制map长度
+			if (list.size() > 999) {
 				int j = (Integer) list.remove(0);
 				map.remove(j);
 				log.warning(map.toString());
 			}
 		}
 	}
+
 	/**
 	 * 返回buf快捷方法。
 	 */
-	private ChannelBuffer backBuffer(ChannelBuffer buf, int _状态码,String msg) {
+	private ChannelBuffer backBuffer(ChannelBuffer buf, int _状态码, String msg) {
 		buf.writeByte(_状态码);
 		buf.writeBytes(Global.getUTF(msg));
 		return buf;
 	}
-	
 }
